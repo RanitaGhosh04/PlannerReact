7b37811910011a19a7611e7247adc40953aee6d1
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-10 23:44:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SubjectManager from './components/SubjectManager'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <SubjectManager/>
+    </>
+  )
+}
+
+export default App
